832a3dfa7062a6112404664babafc969e82ee4f4
sen0rxol0 <sen0rxol0@users.noreply.github.com>
2023-06-27 05:45:38 +0200

    Update restored_external_hax.c
    code refactor, nothing afaik now

--- src/restored_external_hax.c
+++ src/restored_external_hax.c
@@ -10,6 +10,8 @@
 #include "IOKit/IOTypes.h"
 #include "IOUSBDeviceControllerLib.h"
 
+#include "micro_inetd.c"
+
 IOUSBDeviceDescriptionRef IOUSBDeviceDescriptionCreateWithType(CFAllocatorRef, CFStringRef);
 
 /* reversed from restored_external */
@@ -69,59 +71,49 @@ init_mux(void)
     return i;
 }
 
-#include "micro_inetd.c" /* I know, I am a bad person for doing this */
-char *execve_params[] = { "/sbin/sshd", "22", "/sbin/sshd", "-i", NULL };
+void
+disable_watchdog()
+{
+  CFMutableDictionaryRef matching;
+  io_service_t service = 0;
+  matching = IOServiceMatching("IOWatchDogTimer");
+  if (matching == NULL) {
+      printf("unable to create matching dictionary for class IOWatchDogTimer\n");
+  }
+  service = IOServiceGetMatchingService(kIOMasterPortDefault, matching);
+  if (service == 0) {
+      printf("unable to create matching dictionary for class IOWatchDogTimer\n");
+  }
+  uint32_t zero = 0;
+  CFNumberRef n = CFNumberCreate(kCFAllocatorDefault, kCFNumberIntType, &zero);
+  IORegistryEntrySetCFProperties(service, n);
+  IOObjectRelease(service);
+}
 
-int main(int argc, char* argv[]) {
-  printf("Starting  H A K E\n");
-  printf("Most of this made by @xerub");
-  printf("Edited a bit by @daytonhasty");
-    printf("Compiled " __DATE__ " " __TIME__ " using dayt0n's method\n");
-
-    CFMutableDictionaryRef matching;
-    io_service_t service = 0;
-    matching = IOServiceMatching("IOWatchDogTimer");
-    if (matching == NULL) {
-        printf("unable to create matching dictionary for class IOWatchDogTimer\n");
-    }
-
-    service = IOServiceGetMatchingService(kIOMasterPortDefault, matching);
-    if (service == 0) {
-        printf("unable to create matching dictionary for class IOWatchDogTimer\n");
-    }
-    uint32_t zero = 0;
-    CFNumberRef n = CFNumberCreate(kCFAllocatorDefault, kCFNumberIntType, &zero);
-    IORegistryEntrySetCFProperties(service, n);
-    IOObjectRelease(service);
+int
+main(int argc, char *argv[])
+{
 
     if (init_mux()) {
         printf("USB init FAIL\n");
     } else {
-        printf("USB init succeeded, continuing...\n");
+        printf("USB init succeeded, continuing ...\n");
     }
-
-    int i;
-    struct stat st;
-    for (i = 0; i < 10; i++) {
-        printf("Waiting for data partition\n\n");
-        if(!stat("/dev/disk0s2s1", &st)) {
-            break;
-        }
-        if(!stat("/dev/disk0s1s2", &st)) {
-            break;
-        }
-        sleep(5);
-    }
-
+    sleep(1);
+    printf("\n\t\t\t\t\tStarted  H A K E\n");
+    printf("\t\tCredits: @xerub, @daytonhasty\n");
+    printf("\t\tMod by: @sen0rxol0\n");
+    printf("\n\tCompiled: " __DATE__ " " __TIME__ "\n\n");
+    sleep(5);
     printf(" #######  ##    ##\n");
     printf("##     ## ##   ## \n");
     printf("##     ## ##  ##  \n");
     printf("##     ## #####   \n");
     printf("##     ## ##  ##  \n");
     printf("##     ## ##   ## \n");
-    printf(" #######  ##    ##\n\n");
-    printf("greetz from xerub and dayt0n\n");
-    printf("Enjoy your new hax\n");
-    printf("You can connect with an SSH tunnel on port 22\n");
+    printf(" #######  ##    ##\n");
+    printf("\nConnect with an SSH tunnel on port 22\n\n");
+    sleep(3);
+    char *execve_params[] = { "micro_inetd", "22", "/sbin/sshd", "-i", NULL };
     return main2(4, execve_params);
 }
